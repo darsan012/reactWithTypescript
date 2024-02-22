2b462864c6224c75f3bc209e493992df8ac719b5
darsan012 <dsrsangautam@gmail.com>
2024-02-22 14:20:20 -0500

    added the prettierrc.json and added rules

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,21 @@
 export const ListGroup = () => {
-    let cities = ['New York', 'San Francisco', 'Oxford', 'Deutschland']
-    cities = []
+  const cities = ['New York', 'San Francisco', 'Oxford', 'Deutschland'];
 
-    return (
-        <>
-            <h1>List Group</h1>
-            <ul className="list-group">
-                {cities.length === 0 && <p>No items found</p>}
-                {cities.map((city, index) => <li key={index} className="list-group-item">{city}</li>)}
-            </ul>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>List Group</h1>
+      <ul className="list-group">
+        {cities.length === 0 && <p>No items found</p>}
+        {cities.map((city, index) => (
+          <li
+            key={index}
+            className="list-group-item"
+            onClick={() => console.log('clicked')}
+          >
+            {city}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
