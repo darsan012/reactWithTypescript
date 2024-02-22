edb73c1213c9542d271f1a5332477fc3f1220f35
darsan012 <dsrsangautam@gmail.com>
2024-02-22 14:01:43 -0500

    removed the ternary operator and updated the logic by using the logical and statement

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,14 +2,11 @@ export const ListGroup = () => {
     let cities = ['New York', 'San Francisco', 'Oxford', 'Deutschland']
     cities = []
 
-    const getMessage = (message: string) => {
-        return cities.length === 0 ? <p>{message}</p> : null
-    }
     return (
         <>
             <h1>List Group</h1>
             <ul className="list-group">
-                {getMessage('The list is empty')}
+                {cities.length === 0 && <p>No items found</p>}
                 {cities.map((city, index) => <li key={index} className="list-group-item">{city}</li>)}
             </ul>
         </>
