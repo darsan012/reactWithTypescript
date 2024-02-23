dec686e053800ee40fe28711548ca67ddcfdd63a
darsan012 <dsrsangautam@gmail.com>
2024-02-23 01:56:56 -0500

    created functionality to show the list when the button is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-// import { ListGroup } from './components/ListGroup';
+import Button from './components/Button';
+import { ListGroup } from './components/ListGroup';
 
 function App() {
-  // const items = ['New York', 'San Francisco', 'Oxford', 'Deutschland'];
-  // const handleSelectedItem = (item: string) => {
-  //   console.log(item);
-  // };
+  const items = ['New York', 'San Francisco', 'Oxford', 'Deutschland'];
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
+
+  const [btnClick, setBtnClick] = useState(false);
 
   return (
     <>
-      {/* <ListGroup
-        items={items}
-        heading="list"
-        onSelectedItem={handleSelectedItem}
-      /> */}
-      <Alert>
-        Hello <h1>World</h1>
-      </Alert>
+      {/* conditional rendering for displaying alert and list */}
+      {btnClick && (
+        <Alert onClick={() => setBtnClick(false)}>
+          <strong>Click me</strong> to hide cities
+        </Alert>
+      )}
+      {btnClick && (
+        <ListGroup
+          items={items}
+          heading="The list of the cities are:"
+          onSelectedItem={handleSelectedItem}
+        />
+      )}
+      <br />
+      <Button onClick={() => setBtnClick(true)} color="secondary">
+        Show Cities
+      </Button>
     </>
   );
 }
