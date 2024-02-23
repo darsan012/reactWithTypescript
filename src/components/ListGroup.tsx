d7535f58f78307e8865de771ca2f97a2ee608c0e
darsan012 <dsrsangautam@gmail.com>
2024-02-23 00:15:49 -0500

    used active class to highlight the selected list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,27 @@
-import { MouseEvent } from 'react';
+import { useState } from 'react';
 
 export const ListGroup = () => {
   const cities = ['New York', 'San Francisco', 'Oxford', 'Deutschland'];
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List Group</h1>
-      <ul className="list-group">
+      <ul className="list-group ">
         {cities.length === 0 && <p>No items found</p>}
         {cities.map((city, index) => (
-          <li key={index} className="list-group-item" onClick={handleClick}>
+          <li
+            key={index}
+            className={
+              selectedIndex === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+              console.log(selectedIndex);
+            }}
+          >
             {city}
           </li>
         ))}
